9c17862d60e6048c7678104a6efdd65982d5bda0
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-09 23:26:20 +0200

    fix submit for empty description relates #19

--- js/logic.js
+++ js/logic.js
@@ -1,48 +1,43 @@
 const todoFunctions = {
-    generateId: ( () => {
-      let idCounter = 0;
-      function incrementCounter() {
-        return (idCounter += 1);
-      }
-      return incrementCounter;
-    })(),
-
-    cloneArrayOfObjects: (todos) => {
-      return todos.map( (todo) => {
-        return JSON.parse(JSON.stringify(todo));
-      });
-    },
-
-    addTodo: (todos, newTodo) => {
-      return todoFunctions.cloneArrayOfObjects(todos).concat(newTodo)
-    },
-
-    deleteTodo: (todos, idToDelete) => {
-      return todoFunctions.cloneArrayOfObjects(todos).filter(e => e.id != idToDelete)
-    },
-
-    markTodo: (todos, idToMark) => {
-        return todos.map(e => {
-          return {
-            id: e.id,
-            description: e.description,
-            done: (e.id === idToMark) ? true : e.done
-          }
-        })
-    },
-    
-    sortTodos: (todos, sortFunction) => {
-      // stretch goal! Do this last
-      // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
-      // sortFunction will have same signature as the sort function in array.sort
-      // hint: array.slice, array.sort
-    },
-  };
-
-
-  if (typeof module !== 'undefined') {
-    module.exports = {
-      todoFunctions
+  generateId: (() => {
+    let idCounter = 0;
+    function incrementCounter() {
+      return (idCounter += 1);
     }
+    return incrementCounter;
+  })(),
+  cloneArrayOfObjects: todos => {
+    return todos.map(todo => {
+      return JSON.parse(JSON.stringify(todo));
+    });
+  },
+  addTodo: (todos, newTodo) => {
+    return todoFunctions.cloneArrayOfObjects(todos).concat(newTodo);
+  },
+  deleteTodo: (todos, idToDelete) => {
+    return todoFunctions
+      .cloneArrayOfObjects(todos)
+      .filter(e => e.id != idToDelete);
+  },
+  markTodo: (todos, idToMark) => {
+    return todos.map(e => {
+      return {
+        id: e.id,
+        description: e.description,
+        done: e.id === idToMark ? true : e.done
+      };
+    });
+  },
+  sortTodos: (todos, sortFunction) => {
+    // stretch goal! Do this last
+    // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
+    // sortFunction will have same signature as the sort function in array.sort
+    // hint: array.slice, array.sort
   }
-  
+};
+
+if (typeof module !== "undefined") {
+  module.exports = {
+    todoFunctions
+  };
+}
